bc18da5753b87dd39f3f22edd3fbd6a491ed2437
AnwarZahid <anwarzahid.bcs@gmail.com>
2021-03-03 01:01:25 +0500

    Background Thread

---
+++ app/src/main/java/com/example/backgroundthread/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.backgroundthread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.backgroundthread.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+        setContentView(view);
+        activityMainBinding.button.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+       /* BackgroundThread backgroundThread=new BackgroundThread();
+        backgroundThread.start();*/
+        BackgroundThreadRunnable runnable=new BackgroundThreadRunnable();
+        new Thread(runnable).start();
+
+        // application will not responding due to background thread and app will be crash
+        /*for (int i = 0; i <= 10; i++) {
+            Log.d(TAG, "startthread"+i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }*/
+    }
+
+    // handle background Thread using Thread in android
+    public  class BackgroundThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            for (int i = 0; i <= 10; i++) {
+
+                Log.d(TAG, "startthread"+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // handle thread using runnable
+    public class BackgroundThreadRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 10; i++) {
+                Log.d(TAG, "startthread"+i);
+                if(i==7){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            activityMainBinding.button.setText("Wawo");
+                        }
+                    });
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
