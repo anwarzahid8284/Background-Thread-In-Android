4d44b3de7676e8a8608dde5fa2e7a244248b6235
AnwarZahid <anwarzahid.bcs@gmail.com>
2021-03-05 00:20:38 +0500

    Background Thread

--- app/src/main/java/com/example/backgroundthread/MainActivity.java
+++ app/src/main/java/com/example/backgroundthread/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.backgroundthread;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.backgroundthread.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
     ActivityMainBinding activityMainBinding;
+    boolean isStart=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +22,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View view = activityMainBinding.getRoot();
         setContentView(view);
         activityMainBinding.button.setOnClickListener(this::onClick);
+        activityMainBinding.button2.setOnClickListener(this::onClick);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
        /* BackgroundThread backgroundThread=new BackgroundThread();
         backgroundThread.start();*/
-        BackgroundThreadRunnable runnable=new BackgroundThreadRunnable();
-        new Thread(runnable).start();
+
+        switch (v.getId()){
+            case R.id.button:
+                isStart=false;
+                BackgroundThreadRunnable  runnable=new BackgroundThreadRunnable();
+                new Thread(runnable).start();
+                break;
+            case R.id.button2:
+                isStart=true;
+                break;
+
+        }
+
 
         // application will not responding due to background thread and app will be crash
         /*for (int i = 0; i <= 10; i++) {
@@ -46,6 +62,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             super.run();
             for (int i = 0; i <= 10; i++) {
 
+                if(i==7){
+                    activityMainBinding.button.setText("70%");
+                }
                 Log.d(TAG, "startthread"+i);
                 try {
                     Thread.sleep(1000);
@@ -63,6 +82,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void run() {
             for (int i = 0; i <= 10; i++) {
                 Log.d(TAG, "startthread"+i);
+                if(isStart){
+                    return;
+                }
                 if(i==7){
                     runOnUiThread(new Runnable() {
                         @Override
